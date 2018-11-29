80470e699da066ed7fc7ee18ffe7576c3c9a77b8
Bien Vu <tienbienit1992@gmail.com>
2018-11-29 14:24:50 +0700

    RS: remove image style

--- wp-content/themes/riseandshine/functions.php
+++ wp-content/themes/riseandshine/functions.php
@@ -37,9 +37,9 @@ if (function_exists('add_theme_support')) {
 
     // Add Thumbnail Theme Support
     add_theme_support('post-thumbnails');
-    add_image_size('large', 700, '', true); // Large Thumbnail
-    add_image_size('medium', 250, '', true); // Medium Thumbnail
-    add_image_size('small', 120, '', true); // Small Thumbnail
+    // add_image_size('large', 700, '', true); // Large Thumbnail
+    // add_image_size('medium', 250, '', true); // Medium Thumbnail
+    // add_image_size('small', 120, '', true); // Small Thumbnail
     add_image_size('product_list', 600, 420, true); // Product list Thumbnail
     add_image_size('relate_product', 218, 178, true); // Product list Thumbnail
 }
